81bcc9f44c319012abf0a8a321a50b3e3e776794
wilsoncunningham <122071360+wilsoncunningham@users.noreply.github.com>
2025-02-26 19:25:36 +0000

    still working on functions

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -66,8 +66,110 @@ class PasswordsController < ApplicationController
 
   def download_image(url, save_as="image.jpg"):
     urllib.request.urlretrieve(url, save_as)
+  end
+
+  
+  ### Decoding Functions ###
+
+    
+  def text_to_number_first(text: str) -> str:
+    number_string = ""
+    for char in text:
+        if char.isalpha():
+            number_string += ALPHA_NUMER_STRINGS[char.lower()]
+    return number_string
+
+  def image_to_number_first(im) -> str:
+    width, height = im.size
+    mid = height // 2
+    number_str = ""
+    for x in range(width):
+        r,g,b = im.getpixel((x, mid))
+        number_str += str(r+g+b)
+    return number_str
+
+  def number_to_years(number_string: str, n: int) -> list[str]:
+    years = [number_string[i:i+n] for i in range(0, len(number_string), n)]
+    return years
+
+  def years_list_to_doomsdays(years_list: list[str]) -> list[int]:
+    doomsdays = []
+    for year_str in years_list:
+        year = int(year_str)
+        doomsdays.append(doomsday(year))
+    # print(f"These are the doomsdays: {doomsdays}")
+    return doomsdays
+
+  def ddays_modded_joined(ddays_list: list[int]) -> str:
+    """
+    Since the ddays only range from 0 to 6, we will modify the numbers, ensuring
+    variety in all digits. We will do so by (1) joining the ddays into 3-digit
+    numbers, and (2) converting these 3-digit mod7 numbers into mod10. Also, we
+    will take this list of numbers and convert in into a joined string of numbers
+    """
+    ddays_strings = [str(dday) for dday in ddays_list]
+    ddays_joined_str = "".join(ddays_strings)
+    ddays_3s = number_to_years(ddays_joined_str, 3) # a list of 3dig str nums
+    
+    mod10_list = []
+    for mod7 in ddays_3s:
+        mod10_list.append(mod7_mod10(str(mod7)))
+
+    mod10_strings = [str(mod10) for mod10 in mod10_list]
+    modded_joined = "".join(mod10_strings)
+
+    return modded_joined
+
+  def number_str_to_ascii(number_string: str) -> list[int]:
+    ### The characters we want range from 33 to 126 in ASCII codes
+    # So, for simplicity, we will just take 2-digit numbers in the sequence,
+    # and we will negate the number if it is greater than 93 (126-33)
+    doubles = [int(number_string[i:i+2]) for i in range(0, len(number_string), 2)]
+    for number in doubles:
+        if number > 93:
+            doubles.remove(number)
+    ascii_codes = [number + 33 for number in doubles]
+    return ascii_codes
+
+  def ascii_codes_to_password(ascii_codes: list[int]) -> str:
+    password = ""
+    for code in ascii_codes:
+        password += chr(code)
+    return password
+
+
+  # Execution of Decoding #
+
+  def decode(input, complexity: int) -> str:
+    """Given a sample body of text or image, use a special set of pre-dictated
+    rules to reveal the secret code. The complexity is  the number of digits
+    in the years computed in the early steps"""
+
+    if isinstance(input, str):
+        first_number_string = text_to_number_first(input)
+    else:
+        first_number_string = image_to_number_first(input)
+
+    years_list = number_to_years(first_number_string, complexity)
+    doomsdays = years_list_to_doomsdays(years_list)
+    modded_joined = ddays_modded_joined(doomsdays)
+    ascii_codes = number_str_to_ascii(modded_joined)
+
+    password = ascii_codes_to_password(ascii_codes)
+    return password
+
+  def decode_url(url: str, complexity: int) -> str:
+    download_image(url, "image.jpg")
+    img = Image.open("image.jpg")
+    password = decode(img, complexity)
+    return password
+  end
+
+
+
 
 end
+
 
 
 
@@ -91,102 +193,6 @@ def generate_rand_ddays(len: int):
 #####################
 
 
-
-
-
-# Decoding Functions #
-
-def text_to_number_first(text: str) -> str:
-    number_string = ""
-    for char in text:
-        if char.isalpha():
-            number_string += ALPHA_NUMER_STRINGS[char.lower()]
-    return number_string
-
-def image_to_number_first(im) -> str:
-    width, height = im.size
-    mid = height // 2
-    number_str = ""
-    for x in range(width):
-        r,g,b = im.getpixel((x, mid))
-        number_str += str(r+g+b)
-    return number_str
-
-def number_to_years(number_string: str, n: int) -> list[str]:
-    years = [number_string[i:i+n] for i in range(0, len(number_string), n)]
-    return years
-
-def years_list_to_doomsdays(years_list: list[str]) -> list[int]:
-    doomsdays = []
-    for year_str in years_list:
-        year = int(year_str)
-        doomsdays.append(doomsday(year))
-    # print(f"These are the doomsdays: {doomsdays}")
-    return doomsdays
-
-def ddays_modded_joined(ddays_list: list[int]) -> str:
-    """
-    Since the ddays only range from 0 to 6, we will modify the numbers, ensuring
-    variety in all digits. We will do so by (1) joining the ddays into 3-digit
-    numbers, and (2) converting these 3-digit mod7 numbers into mod10. Also, we
-    will take this list of numbers and convert in into a joined string of numbers
-    """
-    ddays_strings = [str(dday) for dday in ddays_list]
-    ddays_joined_str = "".join(ddays_strings)
-    ddays_3s = number_to_years(ddays_joined_str, 3) # a list of 3dig str nums
-    
-    mod10_list = []
-    for mod7 in ddays_3s:
-        mod10_list.append(mod7_mod10(str(mod7)))
-
-    mod10_strings = [str(mod10) for mod10 in mod10_list]
-    modded_joined = "".join(mod10_strings)
-
-    return modded_joined
-
-def number_str_to_ascii(number_string: str) -> list[int]:
-    ### The characters we want range from 33 to 126 in ASCII codes
-    # So, for simplicity, we will just take 2-digit numbers in the sequence,
-    # and we will negate the number if it is greater than 93 (126-33)
-    doubles = [int(number_string[i:i+2]) for i in range(0, len(number_string), 2)]
-    for number in doubles:
-        if number > 93:
-            doubles.remove(number)
-    ascii_codes = [number + 33 for number in doubles]
-    return ascii_codes
-
-def ascii_codes_to_password(ascii_codes: list[int]) -> str:
-    password = ""
-    for code in ascii_codes:
-        password += chr(code)
-    return password
-
-
-# Execution of Decoding #
-
-def decode(input, complexity: int) -> str:
-    """Given a sample body of text or image, use a special set of pre-dictated
-    rules to reveal the secret code. The complexity is  the number of digits
-    in the years computed in the early steps"""
-
-    if isinstance(input, str):
-        first_number_string = text_to_number_first(input)
-    else:
-        first_number_string = image_to_number_first(input)
-
-    years_list = number_to_years(first_number_string, complexity)
-    doomsdays = years_list_to_doomsdays(years_list)
-    modded_joined = ddays_modded_joined(doomsdays)
-    ascii_codes = number_str_to_ascii(modded_joined)
-
-    password = ascii_codes_to_password(ascii_codes)
-    return password
-
-def decode_url(url: str, complexity: int) -> str:
-    download_image(url, "image.jpg")
-    img = Image.open("image.jpg")
-    password = decode(img, complexity)
-    return password
 
 
 #%%
